455a17057ba9eee6779e70e4547cb35b50f40eb9
Francesco Allara <francesco_allara@ipcmedia.com>
2014-08-13 16:45:16 +0100

    download_url is a not found function

--- keystone-wpcli-thumbnails.php
+++ keystone-wpcli-thumbnails.php
@@ -1,5 +1,42 @@
 <?php
 
+if (!function_exists("download_url")) {
+
+	function download_url($url, $timeout = 300) {
+		//WARNING: The file is not automatically deleted, The script must unlink() the file.
+		if (!$url)
+			return new WP_Error('http_no_url', __('Invalid URL Provided.'));
+
+		$tmpfname = wp_tempnam($url);
+		if (!$tmpfname)
+			return new WP_Error('http_no_file', __('Could not create Temporary file.'));
+
+		$response = wp_safe_remote_get($url, array('timeout' => $timeout, 'stream' => true, 'filename' => $tmpfname));
+
+		if (is_wp_error($response)) {
+			unlink($tmpfname);
+			return $response;
+		}
+
+		if (200 != wp_remote_retrieve_response_code($response)) {
+			unlink($tmpfname);
+			return new WP_Error('http_404', trim(wp_remote_retrieve_response_message($response)));
+		}
+
+		$content_md5 = wp_remote_retrieve_header($response, 'content-md5');
+		if ($content_md5) {
+			$md5_check = verify_file_md5($tmpfname, $content_md5);
+			if (is_wp_error($md5_check)) {
+				unlink($tmpfname);
+				return $md5_check;
+			}
+		}
+
+		return $tmpfname;
+	}
+
+}
+
 class Keystone_WpCli_Thumbnails {
 
 	/**
@@ -8,14 +45,14 @@ class Keystone_WpCli_Thumbnails {
 	 * @var string
 	 */
 	protected $_url;
-	
+
 	/**
 	 * The local requested file
 	 * 
 	 * @var string
 	 */
 	protected $_file;
-	
+
 	/**
 	 * Initialize the class that needs to handle the callback
 	 */
@@ -51,28 +88,28 @@ class Keystone_WpCli_Thumbnails {
 		$this->_file = $file;
 
 		$has_errors = false;
-		
+
 		if ($has_errors == false && !$this->is_requesting_local_file()) {
 			$has_errors = true;
 		}
-		
+
 		if ($has_errors == false && !$this->parent_dir_exists()) {
 			$has_errors = true;
 		}
-		
+
 		$this->_url = $this->get_url();
-		if($has_errors == false && empty(trim($this->_url))) {
+		if ($has_errors == false && empty(trim($this->_url))) {
 			$has_errors = true;
 		}
-		
-		if($has_errors == false) {
+
+		if ($has_errors == false) {
 			$download = $this->download();
-			if(is_wp_error($download)) {
+			if (is_wp_error($download)) {
 				return $this->_file;
 			}
 			rename($download, $this->_file);
 		}
-		
+
 		return $this->_file;
 	}
 
@@ -99,7 +136,7 @@ class Keystone_WpCli_Thumbnails {
 		}
 		return $exists;
 	}
-	
+
 	/**
 	 * Returns the URL starting from the local path requested
 	 * 
@@ -108,7 +145,7 @@ class Keystone_WpCli_Thumbnails {
 	public function get_url() {
 		return sprintf("http://inspire-ipcmedia-com.s3-eu-west-1.amazonaws.com/inspirewp/live%s", substr($this->_file, stripos($this->_file, "/wp-content/")));
 	}
-	
+
 	/**
 	 * Downloads a file from the s3 instance using the WP built in functions
 	 * 
@@ -117,5 +154,5 @@ class Keystone_WpCli_Thumbnails {
 	public function download() {
 		return download_url($this->_url);
 	}
-}
 
+}
