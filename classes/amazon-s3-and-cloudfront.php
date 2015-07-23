f62dd132aca41f7019e15fd02bb85fc47081557e
Daniel Homer <danielhomer@me.com>
2015-07-23 16:10:53 +0100

    Suppress warnings on falsey path

--- classes/amazon-s3-and-cloudfront.php
+++ classes/amazon-s3-and-cloudfront.php
@@ -88,11 +88,12 @@ class Amazon_S3_And_CloudFront extends AWS_Plugin_Base {
 		$exists = true;
 		$dir = $this->get_parent_dir( $file );
 		
-		if ( ! is_dir( $dir ) ) {
-			var_dump( $dir );
-			var_dump( $file );
+		if ( $dir && ! is_dir( $dir ) ) {
 			$exists = mkdir( $dir, 0755, true );
+		} else if ( ! $dir ) {
+			$exists = false;
 		}
+
 		return $exists;
 	}
 
